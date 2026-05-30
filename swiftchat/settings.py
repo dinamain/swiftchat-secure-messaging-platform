8863bfed7775bd3bc4ca09db448a9fb6032c4d4e
Dina <dinausman2001@gmail.com>
2026-05-30 22:08:02 +0530

    Migrate database from SQLite to PostgreSQL using Docker

--- swiftchat/settings.py
+++ swiftchat/settings.py
@@ -89,9 +89,13 @@ WSGI_APPLICATION = 'swiftchat.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "swiftchat",
+        "USER": "swiftchat_user",
+        "PASSWORD": "swiftchat_password",
+        "HOST": "127.0.0.1",
+        "PORT": "5433",
     }
 }
 
