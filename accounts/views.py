65c55702fa7295b02d3f6df88a1edfc3c17703c4
Dina <dinausman2001@gmail.com>
2026-06-12 10:34:46 +0530

    UserListview done

--- accounts/views.py
+++ accounts/views.py
@@ -70,3 +70,24 @@ class CustomLoginView(APIView):
                 status=status.HTTP_200_OK,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class UserListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        query = request.GET.get("q", "")
+
+        users = User.objects.exclude(id=request.user.id)
+
+        if query:
+            users = users.filter(
+                Q(username__icontains=query) | Q(email__icontains=query)
+            )
+
+        data = [
+            {"id": u.id, "username": u.username, "email": u.email}
+            for u in users
+        ]
+
+        return Response(data)
