26707ce08ae29e57811dc6cce0557beebce41c62
Dina <dinausman2001@gmail.com>
2026-06-17 13:45:55 +0530

    Fix missing import in notifications views

--- notifications/views.py
+++ notifications/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 from .models import Notification
 from .serializers import NotificationSerializer
@@ -24,7 +25,6 @@ class NotificationListView(APIView):
         )
 
         return Response(serializer.data)
-    from django.shortcuts import get_object_or_404
 
 
 class MarkNotificationReadView(APIView):
